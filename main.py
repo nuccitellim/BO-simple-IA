bbdb1d08fb2ca0103b986ed8a362aa736aae1c36
nuccitellim <m-nuccitelli@hotmail.com>
2024-12-11 22:56:29 +0000

    Nueva interfaz y funcionalidad de limpiar búsqueda

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+from logging import PlaceHolder
 import streamlit as st
 import os
 import requests
 import json
 
-def decodificar_json(objeto):
+my_secret = os.environ['DIFY_API_KEY']
+
+
+def decodificar_json(response):
     """ Recibe objeto json y retorna campo respuesta"""
     response_str = response.content.decode('utf-8')
     response_dict = json.loads(response_str)
@@ -11,39 +15,80 @@ def decodificar_json(objeto):
     return answer
 
 
-my_secret = os.environ['DIFY_API_KEY']
+# Configuración de la página
+st.set_page_config(page_title="Boletín Oficial Simple",
+                   page_icon="📃",
+                   layout="wide")
 
-# Título
-st.title("Boletín Oficial Simple")
-st.subheader("Inteligencia Artificial Generativa para simplificar las normas")
-# Descripción
-st.write(
-    "Este es un Asistente que utiliza Inteligencia Artificial Generativa para simplificar las normas publicadas a diario en el Boletín Oficial de tu jurisdicción."
-)
-# Detalles sobre el funcionamiento
-st.write("""
-### Puedes simplificar:
-- Leyes
-- Decretos
-- Resoluciones
-- Disposiciones
-- Otros actos administrativos
-      
-""")
+# CSS para personalizar estilos
+st.markdown("""
+    <style>
+    .centered-title {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 3em;
+        font-weight: bold;
+        margin-bottom: 8px;
+    }
 
-st.write("---")
+    .centered-subtitle {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 1.5em;
+        font-weight: normal;
+        margin-bottom: 50px;
+    }
+    .custom-label {
+        font-size: 1.1em;
+        margin-bottom: 10px;
+    }
+    .right-column {
+        margin-left: 50px; 
+    }
+    .footer {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-top: 50px;
+        font-size: 1.2em;
+    }
+    </style>
+    """,
+            unsafe_allow_html=True)
 
-st.write(
-    "Por favor, copia el texto de la norma que deseas simplificar y luego pega el contenido aquí."
-)
+# Inicializar variables en el estado de la sesión
+if 'assistant_response' not in st.session_state:
+    st.session_state.assistant_response = ""
 
-# Campo de entrada para la norma
-norma = st.text_area("Pega la norma aquí:", height=300)
+if 'norm_text_area' not in st.session_state:
+    st.session_state.norm_text_area = ""
 
-# Crear un botón para enviar el mensaje
-if st.button("Generar resumen"):
-    # Si el botón es presionado, mostrar el texto ingresado en la sección de resultado
-    st.write("### Resultado:")
+# Título y subtítulo
+st.markdown('<div class="centered-title"> 📃 Boletín Oficial Simple 📃</div>',
+            unsafe_allow_html=True)
+st.markdown(
+    '<div class="centered-subtitle"> Inteligencia Artificial Generativa para simplificar las normas</div>',
+    unsafe_allow_html=True)
+
+# Dividir en columnas
+col1, col2 = st.columns([5, 5])
+
+with col1:
+    st.markdown(
+        '<div class="custom-label">Pega el texto de la norma que desees simplificar: </div>',
+        unsafe_allow_html=True)
+
+    # Vincular el área de texto con el estado de la sesión
+    st.session_state.norm_text_area = st.text_area(
+        label="",
+        value=st.session_state.norm_text_area,
+        placeholder="Pega el texto de la norma aquí.",
+        height=300,
+    )
+
+    # Configurar los datos de la solicitud
     base_url = "https://api.dify.ai/v1"
     path = "/completion-messages"
     full_path = base_url + path
@@ -53,14 +98,66 @@ if st.button("Generar resumen"):
     }
     data = {
         "inputs": {
-            "topic": f"Bearer {norma}"
+            "topic": st.session_state.norm_text_area
         },
         "response_mode": "blocking",
         "user": "m-nuccitelli@hotmail.com"
     }
 
-    response = requests.post(full_path, json=data, headers=headers)
+    # Botón para generar el resumen
+    gresumen = st.button("Generar resumen")
+    if gresumen:
+        if st.session_state.norm_text_area:
+            with st.spinner("Procesando la norma..."):
+                response = requests.post(full_path, json=data, headers=headers)
+                if response.status_code == 200:
+                    answer = decodificar_json(response)
+                    st.session_state.assistant_response = answer
+                else:
+                    st.error(
+                        "Hubo un error al generar la respuesta. Intenta nuevamente."
+                    )
 
-    # Mostrar la respuesta generada por el modelo en un campo de texto
-    answer = decodificar_json(response)
-    st.write(answer, height=200)
+        # Definir la función para limpiar la búsqueda
+        def limpiar_busqueda():
+            st.session_state.norm_text_area = ""  # Limpiar el área de texto
+            st.session_state.assistant_response = ""  # Limpiar la respuesta del asistente
+
+        # Botón para limpiar búsqueda con callback
+        st.button("Limpiar búsqueda", on_click=limpiar_busqueda)
+
+with col2:
+    # Mostrar instrucciones o respuesta
+    if not st.session_state.assistant_response:
+        st.markdown('''
+        <div class="right-column">
+            <h3>Instrucciones</h3>
+            <p>🤖 Este es tu nuevo Asistente con Inteligencia Artificial Generativa, pensado para hacerte la vida más fácil.
+
+📜 Se encarga de simplificar las normas publicadas todos los días en el Boletín Oficial de tu jurisdicción. Podés resumir:
+- 🏛️ Leyes  
+- 📄 Decretos  
+- 🖋️ Resoluciones  
+- 📑 Disposiciones  
+- ⚖️ Cualquier otro acto administrativo  
+
+Solo escribí el texto que querés simplificar, hacé clic en "Generar resumen" ¡y listo!✨</p>
+        </div>
+        ''',
+                    unsafe_allow_html=True)
+    else:
+        st.markdown('''
+        <div class="right-column">
+            <h4>Respuesta:</h4>
+            <p>{}</p>
+        </div>
+        '''.format(st.session_state.assistant_response),
+                    unsafe_allow_html=True)
+
+# Pie de página
+st.markdown(
+    '<div class="footer"><p>BO Simple puede cometer errores. Considera verificar la información importante.</p></div>',
+    unsafe_allow_html=True)
+st.markdown(
+    '<div class="footer"><a href="https://forms.gle/mYQFmMyQA7fMMYtF9" target="_blank">Hacé click acá para dejar comentarios, sugerencias o reportes sobre el asistente.</a></div>',
+    unsafe_allow_html=True)
